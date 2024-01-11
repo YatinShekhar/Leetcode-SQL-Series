adab6f57477f610f30992c7639521aa240404227
Yatin Shekhar Rikhari <121398971+YatinShekhar@users.noreply.github.com>
2024-01-11 22:35:53 +0530

    Add files via upload

---
+++ Basic-Windowing-Functions/Product_Sales_Analysis_3.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/product-sales-analysis-iii/
+
+with cte as (
+select * ,  dense_rank() over(partition by product_id order by year) as rn
+from sales )
+select product_id , year as first_year , quantity , price
+from cte
+where rn = 1 
