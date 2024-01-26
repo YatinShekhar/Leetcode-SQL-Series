189452d06393599e92fc7c6b6308ac8fbb1cf4cf
Yatin Shekhar Rikhari <121398971+YatinShekhar@users.noreply.github.com>
2024-01-26 09:02:35 +0530

    Add files via upload

---
+++ Basic-Joins/Project_Employees_1.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/project-employees-i/
+
+select project_id , round(avg(experience_years),2) as average_years
+from project p left join employee e
+using(employee_id)
+group by 1
